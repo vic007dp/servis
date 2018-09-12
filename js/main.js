309aff05b4cd350c8f14035dbdc680e0117642ef
Victor <vic007.dp@gmail.com>
2018-09-12 17:04:11 +0300

    slader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(function (){
+    //Бегущая строка
     $(window).load(function(){
         $('.str').liMarquee();
     });
+    //Слайдер
+     $('.slader').slick({
+        infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+     });
 });
