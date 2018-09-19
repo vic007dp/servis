7790e918a6d779567288061ddffe0e33c0282166
Victor <vic007.dp@gmail.com>
2018-09-19 14:23:55 +0300

    sl-adaptiv

--- js/main.js
+++ js/main.js
@@ -10,5 +10,29 @@ $(function (){
           slidesToScroll: 4,
          prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
          nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+         responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+             ]
      });
-});
+             });
